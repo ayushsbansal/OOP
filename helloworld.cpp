93b3472e35d86c86da0f85c9ebf2dc2cec935ab1
Ayush Bansal <47652982+ayushsbansal@users.noreply.github.com>
2020-09-19 00:30:10 +0530

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,14 @@
-#include<iostream>
-using namespace std;
+#include<iostream> //declarations for cin and cout objects
+// # -> Preprocessor directive
+using namespace std;	
+// using and namespace are keywords
+//std contains definitions for cin and cout objects
 
-int main(){
+int main(){	
+	//main is a function
+	//execution starts at main
+	//Every program must have main function
 	cout<<"Hello World";
+	//Prints Hello World to the screen
+	
 }
